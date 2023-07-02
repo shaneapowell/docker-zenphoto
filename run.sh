058aca2477bac22477e6bb4614fa13828ed4da46
Shane Powell <shaneapowell@hotmail.com>
2023-07-02 14:20:42 -0500

    .

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ mkdir -p /var/www/data/cache/cache
 mkdir -p /var/www/data/cache/cache_html
 
 chown www-data /var/www/data/zp-data
-chown www-data /var/www/data/cache
+chown -R www-data /var/www/data/cache
 
 /usr/local/bin/apache2-foreground
