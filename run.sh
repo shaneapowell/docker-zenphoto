a0791addf88935868664bfe43bdeac5971db9919
Shane Powell <shaneapowell@hotmail.com>
2023-07-03 09:32:29 -0500

    Can now be correctly configured to run in a sub-path

--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@ mkdir -p /var/www/data/albums
 mkdir -p /var/www/data/cache/cache
 mkdir -p /var/www/data/cache/cache_html
 
+echo $ZP_PATH
+if [ -n "${ZP_PATH}" ]; then
+    echo "Creating sub-path link ${ZP_PATH}"
+    ln -s /var/www/html /var/www/html/${ZP_PATH}
+fi
+
 chown www-data /var/www/data/zp-data
 chown -R www-data /var/www/data/cache
 
